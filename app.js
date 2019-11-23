02a37c5d676c2ab53b80cfb866028534447c16ac
Alexandre Ferreira <34192221+magno193@users.noreply.github.com>
2019-11-22 23:00:50 -0300

    Objetos e propriedades

--- app.js
+++ app.js
@@ -1,8 +1,25 @@
-// Default value legacy
-function greet(name) {
-    name = name || '<Seu nome>'
-    console.log('Olá ' + name);
-}
+// Objeto
+// Objeto é uma coleção de valores 
+// que podem ter outras coleções de valores
+var person = new Object();
 
-greet('Alex');
-greet();
+// Propriedades
+person['firstname'] = 'Alexandre';
+person['lastname'] = 'Ferreira';
+
+var firstNameProperty = 'firstname'
+
+console.log(person); // Objeto
+console.log(person[firstNameProperty]); // Acesso computado de propriedade
+
+console.log(person.firstname); // Acesso de propriedade
+console.log(person.lastname); // Acesso de propriedade
+
+person.address = new Object(); // Objeto dentro de objeto
+person.address.street = 'General Telles, 2877';
+person.address.city = 'Botucatu';
+person.address.state = 'SP';
+
+console.log(person.address.street);
+console.log(person.address.city);
+console.log(person['address']['state']);
