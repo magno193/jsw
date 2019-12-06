b6aba059be85f2880c79c8c35bfa76dbc889ce3a
magno193 <34192221+magno193@users.noreply.github.com>
2019-12-06 00:03:05 -0200

    Default value legacy

--- app.js
+++ app.js
@@ -1,12 +1,8 @@
-// Retorna primeiro (false < 1) depois true pois false = 0
-console.log(3 < 2 < 1);
+// Default value legacy
+function greet(name) {
+    name = name || '<Seu nome>'
+    console.log('Olá ' + name);
+}
 
-//Coerção
-console.log(Number(true));
-console.log(Number(false));
-console.log(Number(null));
-console.log(Number(undefined));
-
-// Igualdade com coerção e igualdade restrita
-console.log('3' == 3); // Não recomendado
-console.log('3' === 3); 
+greet('Alex');
+greet();
