aecb3b295ebe7bdd3829de90e32f9eef48a652d0
magno193 <34192221+magno193@users.noreply.github.com>
2019-12-08 16:31:37 -0200

    Function expression e statement

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
-// Funções como Objetos - em javascript funções são objetos.
-// First class functions - tudo que se pode fazer com outros types
-// você pdoe fazer com funções.
+// Function expression - uma unidade de código que retorna um resultado.
 
-// Function é um object especial, que se pode passar vários tipos de dados
-// como primitivos, objetos, funções.
+// Function statement - uma unidade de código que não retorna um resultado.
 
+// Statement - não retorna um objeto, alocado em memória antes do contexto
+// de execução.
 function greet() {
-    console.log('Oi');
+    console.log('oi');
 }
 
-// Como function é um object, pode-se passar propriedades
-greet.language = 'portuguese';
-console.log(greet);
-console.log(greet.language);
-console.log(greet());
+// Expression - retorna o objeto anonymousGreet no contexto de execução.
+var anonymousGreet = () => {
+    console.log('oi');
+}
+
+greet();
+anonymousGreet();
+
+// Programação funcional - passando função dentro de função
+function log(a) {
+    console.log(a)
+    a();
+}
+
+log(() => {
+    console.log('hi');
+});
