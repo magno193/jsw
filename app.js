2f9c40bf6cb2706b3236312dff36910e06c0b20d
Alexandre Ferreira <alexmlf93@gmail.com>
2019-12-09 01:09:23 -0300

    Objeto global 'this'

--- app.js
+++ app.js
@@ -1,29 +1,36 @@
-// Por valor (primitivos)
-var a = 3;
-var b;
-
-// Cria novo local na memória para b, sendo uma cópia de a
-b = a;
-a = 2
-console.log(a, b);
-
-
-// Por referência (todos os objetos (inclusive funções))
-var c = { greeting: 'hi' };
-var d;
-
-// Aponta para mesma referência
-d = c;
-c.greeting = 'hello';
-console.log(c, d);
-
-function changeGreeting(obj) {
-    obj.greeting = 'Olá'
+function a() {
+    console.log(this)
 }
 
-changeGreeting(d);
-console.log(c, d);
+var b = function() {
+    console.log(this);
+    this.newvariable = 'olá';
+}
 
-// O operador = cria novo endereço em memória
-c = { greeting: 'howdy' }
-console.log(c, d)
+// this aponta para o mesmo endereço
+console.log(this);
+a();
+b();
+console.log(newvariable);
+
+// Quando this é chamado por um método ele se refere ao objeto
+// no qual ele pertence.
+var c = {
+    name: 'O objeto c',
+    // Método
+    log: function() {
+        var self = this;
+
+        self.name = 'Atualizado o objeto c'
+        console.log(self);
+
+        var setname = function(newname) {
+            self.name = newname;
+        }
+
+        setname('Atualizando novamento o objeto c');
+        console.log(self);
+    }
+}
+console.log(c);
+c.log();
