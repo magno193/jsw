62f6908f82a9fd001fc8938f390d03545a2bc3c8
magno193 <34192221+magno193@users.noreply.github.com>
2019-12-13 23:09:26 -0200

    Arrays

--- app.js
+++ app.js
@@ -1,36 +1,16 @@
-function a() {
-    console.log(this)
-}
+// Array é uma coleção
+// Em javascript o array pode ser de vários tipos diferentes
+var arr = [
+    1,
+    false,
+    {name: 'Alexandre', address: 'General Telles'},
+    function (name) {
+        var greeting = 'Olá ';
+        console.log(greeting + name);
+    },
+    "Olá"
+];
 
-var b = function() {
-    console.log(this);
-    this.newvariable = 'olá';
-}
-
-// this aponta para o mesmo endereço
-console.log(this);
-a();
-b();
-console.log(newvariable);
-
-// Quando this é chamado por um método ele se refere ao objeto
-// no qual ele pertence.
-var c = {
-    name: 'O objeto c',
-    // Método
-    log: function() {
-        var self = this;
-
-        self.name = 'Atualizado o objeto c'
-        console.log(self);
-
-        var setname = function(newname) {
-            self.name = newname;
-        }
-
-        setname('Atualizando novamento o objeto c');
-        console.log(self);
-    }
-}
-console.log(c);
-c.log();
+console.log(arr);
+// Invocando função dentro do array
+arr[3](arr[2].name)
