fc1fe8944986e399aadf129f860e60509e7e826b
Alexandre Ferreira <alexmlf93@gmail.com>
2020-01-13 02:48:06 -0300

    Fábricas de funções

--- app.js
+++ app.js
@@ -1,53 +1,20 @@
-// Exemplo de closure
-function buildFunctions() {
-    var arr = [];
+function makeGreeting(language) {
 
-    for (var i = 0; i < 3; i++) {
+    return function (firstname, lastname) {
+        
+        if (language === 'pt') {
+            console.log(`Olá ${firstname} ${lastname}`);            
+        }
 
-        arr.push(
-            // Não executado mas sim armazenado
-            function () {
-                console.log(i)
-            }
-        )
+        if (language === 'en') {
+            console.log(`Hello ${firstname} ${lastname}`);
+        }
     }
-
-    return arr;
 }
 
+var greetPortuguese = makeGreeting('pt');
+var greetEnglish = makeGreeting('en');
 
-var fs = buildFunctions();
-
-fs[0]();
-fs[1]();
-fs[2]();
-
-// Todas retornam 3 porque o ultimo valor de i é 3
-// e i é o que está em memória no momento que as funções
-// fs[]() são executadas
-
-
-// Forma de corrigir o problema
-function buildFunctions() {
-    var arr = [];
-
-    for (var i = 0; i < 3; i++) {
-        arr.push(
-            // Executando a função passando i
-            (function (j) {
-                return function () {
-                    console.log(j);
-                }
-            }(i))
-        )
-    }
-
-    return arr;
-}
-
-
-var fs2 = buildFunctions2();
-
-fs[0]();
-fs[1]();
-fs[2]();
+// Aponta language para o contexto de execução anterior
+greetPortuguese('Alex', 'Ferreira');
+greetEnglish('John', 'Doe');
