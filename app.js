4a4fceebb9827b04bf45c90b51b528624b04ed2f
Alexandre Ferreira <alexmlf93@gmail.com>
2020-01-13 02:32:30 -0300

    Closure pt2

--- app.js
+++ app.js
@@ -1,15 +1,53 @@
-function greet(whattosay) {
-    
-    return function (name) {
-        console.log(whattosay + ' ' + name);
+// Exemplo de closure
+function buildFunctions() {
+    var arr = [];
+
+    for (var i = 0; i < 3; i++) {
+
+        arr.push(
+            // Não executado mas sim armazenado
+            function () {
+                console.log(i)
+            }
+        )
     }
 
+    return arr;
 }
 
-// Invocando uma função dentro de uma função
-greet('Hi')('Tony');
 
-// Armazenando a função invocada
-var sayHi = greet('Hi');
-// Closure - pega a variavel da função fechada anterior como escopo global
-sayHi('Tony');
+var fs = buildFunctions();
+
+fs[0]();
+fs[1]();
+fs[2]();
+
+// Todas retornam 3 porque o ultimo valor de i é 3
+// e i é o que está em memória no momento que as funções
+// fs[]() são executadas
+
+
+// Forma de corrigir o problema
+function buildFunctions() {
+    var arr = [];
+
+    for (var i = 0; i < 3; i++) {
+        arr.push(
+            // Executando a função passando i
+            (function (j) {
+                return function () {
+                    console.log(j);
+                }
+            }(i))
+        )
+    }
+
+    return arr;
+}
+
+
+var fs2 = buildFunctions2();
+
+fs[0]();
+fs[1]();
+fs[2]();
